be62ab707d2dfb35db710e941ccd75cd80f4da4e
FezileChelebiyeva <fezile.celebiyevaa@gmail.com>
2022-12-29 12:13:38 +0400

    edit

--- blogapp/src/pages/details/index.jsx
+++ blogapp/src/pages/details/index.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "./index.scss";
 import axios from "axios";
 import { Helmet } from "react-helmet";
 
 const DetailsPage = () => {
+  const input = useRef();
   const { id } = useParams();
   const navigate = useNavigate();
-  const [blog, setBlog] = useState([]);
+  const [blog, setBlog] = useState({});
+  const [editStatus, setEditStatus] = useState(false);
   useEffect(() => {
     axios
       .get(`http://localhost:8000/blogs/${id}`)
@@ -20,6 +22,9 @@ const DetailsPage = () => {
       .then((response) => setBlog(response.data))
       .then(() => navigate("/"));
   };
+  const handleEdit = (e) => {
+    editStatus ? setEditStatus(false) : setEditStatus(true);
+  };
 
   return (
     <div id="details">
@@ -31,6 +36,7 @@ const DetailsPage = () => {
 
       <div className="detail-card">
         <h3>{blog.blogTitle}</h3>
+        <p>{blog.blogBody}</p>
         <p>
           <i>Written {blog.blogAuthor}</i>
         </p>
@@ -42,8 +48,25 @@ const DetailsPage = () => {
           >
             Delete
           </button>
+          <button
+            id={blog.id}
+            onClick={(e) => handleEdit(e)}
+            className="deleteBtn"
+          >
+            Edit
+          </button>
         </div>
       </div>
+      {editStatus ? (
+        <div>
+          <div>
+          <input value={blog.blogTitle} type="text" />
+          </div>
+          <div>
+          <input type="text" />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
