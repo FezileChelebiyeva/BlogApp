1fc9b236bd6b41da5ac921d9e7c683eaa3d77afa
FezileChelebiyeva <fezile.celebiyevaa@gmail.com>
2023-01-13 12:20:03 +0400

    form added

--- blogapp/src/pages/details/index.jsx
+++ blogapp/src/pages/details/index.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import "./index.scss";
 import axios from "axios";
 import { Helmet } from "react-helmet";
-
+import { Button, Form, Input, Select } from "antd";
 const DetailsPage = () => {
   const input = useRef();
   const textarea = useRef();
@@ -24,18 +24,16 @@ const DetailsPage = () => {
       .then((response) => setBlog(response.data))
       .then(() => navigate("/"));
   };
-  // const handleEdit = (e) => {
-  //   editStatus ? setEditStatus(false) : setEditStatus(true);
-  //   input.current.value = blog.blogTitle 
-  // };
-  // const handleSubmit = (e) => {
-  //   let obj = {
-  //     blogTitle: input.current.value,
-  //     blogBody: textarea.current.value,
-  //     blogAuthor: select.current.value,
-  //   }
-  //   console.log(obj);
-  // };
+  const handleEdit = (e) => {
+    editStatus ? setEditStatus(false) : setEditStatus(true);
+  };
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
 
   return (
     <div id="details">
@@ -59,33 +57,87 @@ const DetailsPage = () => {
           >
             Delete
           </button>
-          {/* <button
+          <button
             id={blog.id}
             onClick={(e) => handleEdit(e)}
             className="deleteBtn"
           >
             Edit
-          </button> */}
+          </button>
         </div>
       </div>
-      {/* {editStatus ? (
+      {editStatus ? (
         <div>
-          <div>
-            <input ref={input} type="text" />
-          </div>
-          <div>
-            <textarea ref={textarea} cols="40" rows="2" value={blog.blogBody} type="text" />
-            <br />
-            <select ref={select} value={blog.blogAuthor} name="" id="">
-              <option value="kenan">kenan</option>
-              <option value="gurban">gurban</option>
-              <option value="fezile">fezile</option>
-            </select>
-            <br /><br />
-            <button onClick={() => handleSubmit()}>Submit</button>
-          </div>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Blog title:"
+              name="blogTitle"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your blog title!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Blog body:"
+              name="blogBody"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your blog body!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Blog author:"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your blog body!",
+                },
+              ]}
+            >
+              <Select>
+                <Select.Option value="kenan">kenan</Select.Option>
+                <Select.Option value="fezile">fezile</Select.Option>
+                <Select.Option value="gurban">gurban</Select.Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
-      ) : null} */}
+      ) : null}
     </div>
   );
 };
